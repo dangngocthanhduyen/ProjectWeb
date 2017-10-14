4d0ff1d112297b20572f56726d6707d66901373a
duongnguyen2110 <meocom10@gmail.com>
2017-10-14 23:16:37 +0700

    edit icon hover,fix item search,connect link

--- home/js/js.js
+++ home/js/js.js
@@ -4,7 +4,14 @@ $(document).ready(function(){
 	    $(".navbar-toggle").click(function(){
         $(".collapse").toggle();
     });
-	
+	hover(".lip-stick");
+	unhover(".lip-stick");
 });
 
 //-Function---
+function hover(element) {
+    element.setAttribute('src', '../image/banner/lipstick-icon.png');
+}
+function unhover(element) {
+    element.setAttribute('src', '../image/banner/lipstick-icon.png');
+}
